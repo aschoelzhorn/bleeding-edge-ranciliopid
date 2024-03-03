88f791acd619c4fc323ae9a9f898cd02a8386a5e
Alexander Schölzhorn <alexander.sch@gmx.at>
2024-03-03 16:07:13 +0100

    add code snippet for wifi signal strength

--- rancilio-pid/rancilio-network.h
+++ rancilio-pid/rancilio-network.h
@@ -11,6 +11,37 @@ bool isWifiWorking();
 
 extern bool forceOffline;
 
-extern int8_t wifi_rssi; // -120 to 0 (0 is best, -90 to -120 unusable)
+extern int8_t wifi_rssi; // -120 to 0 (0 is best, -90 to -120 unusable), 
+/*
+void getSignalStrength() {
+    if (offlineMode == 1) return;
+
+    long rssi;
+
+    if (WiFi.status() == WL_CONNECTED) {
+        rssi = WiFi.RSSI();
+    }
+    else {
+        rssi = -100;
+    }
+
+    if (rssi >= -50) {
+        signalBars = 4;
+    }
+    else if (rssi < -50 && rssi >= -65) {
+        signalBars = 3;
+    }
+    else if (rssi < -65 && rssi >= -75) {
+        signalBars = 2;
+    }
+    else if (rssi < -75 && rssi >= -80) {
+        signalBars = 1;
+    }
+    else {
+        signalBars = 0;
+    }
+}
+
+*/
 
 #endif
